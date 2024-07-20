0daca1ff1b7d180ee357acc3b0becd2fcaa325f8
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-07-20 02:08:45 -0700

    updates store

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,10 +1,14 @@
 import './Canvas.scss'
 import Node from '../Node/Node'
+import { useDirectedGraph } from '../../lib/hooks/useDirectedGraph'
 
 export default function Canvas() {
+
+    const { } = useDirectedGraph();
+
   return (
     <section id='Canvas'>
-        <Node />
+        
     </section>
   )
 }
--- src/lib/hooks/useDirectedGraph.ts
+++ src/lib/hooks/useDirectedGraph.ts
@@ -1,8 +1,30 @@
-import DirectedGraph, { Node, Edge } from '../../lib/directedGraph'
+import { create } from 'zustand'
 
-export function useDirectedGraph() {
-    const graph = new DirectedGraph();
-    graph.addNode({
-        id: ''
-    })
+interface Node {
+    id: string;
+    value?: string | number;
 }
+
+interface Edge {
+    from: string;
+    to: string;
+}
+
+function generateRandomStringId(): string {
+  const length = 16;
+  const array = new Uint8Array(length / 2);
+  window.crypto.getRandomValues(array);
+  return Array.from(array, (byte) => byte.toString(16).padStart(2, "0")).join(
+    ""
+  );
+}
+
+export const useDirectedGraph = create((set) => ({
+    graph: {
+        nodes: new Map<string, Node>(),
+        adjacencyList: new Map<string, Edge[]>()
+    },
+    addNode: (node: Node) =>{
+        
+    }
+}))
