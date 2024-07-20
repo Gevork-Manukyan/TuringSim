a22d477047c7ef597447f85f6b5df2b6a5a5b5d4
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-07-20 02:08:29 -0700

    works on useDirectedGraph store

---
+++ src/lib/hooks/useDirectedGraph.ts
@@ -0,0 +1,8 @@
+import DirectedGraph, { Node, Edge } from '../../lib/directedGraph'
+
+export function useDirectedGraph() {
+    const graph = new DirectedGraph();
+    graph.addNode({
+        id: ''
+    })
+}
