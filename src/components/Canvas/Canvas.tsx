f044b64a5fe9552830a545e04b0abd7f2e72a44a
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-07-21 00:58:21 -0700

    works on useDirectedGraph

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,11 +1,9 @@
 import './Canvas.scss'
-import { useDirectedGraph } from '../../lib/hooks/useDirectedGraph'
 import Node from '../Nodes/Node/Node'
 import StartNode from '../Nodes/EndNode/EndNode'
 
 export default function Canvas() {
 
-    const { } = useDirectedGraph();
 
   return (
     <section id='Canvas'>
--- src/lib/hooks/useDirectedGraph.ts
+++ src/lib/hooks/useDirectedGraph.ts
@@ -1,13 +1,19 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
-interface Node {
-    id: string;
-    value?: string | number;
+type Node = {
+  id: string;
+  value?: string | number;
 }
 
-interface Edge {
-    from: string;
-    to: string;
+type Edge = {
+  from: string;
+  to: string;
+}
+
+type TUseDirectedGraph = {
+  nodes: Map<string, Node>;
+  adjacencyList: Map<string, Edge[]>;
+  addNode: (node: Node) => TUseDirectedGraph | void;
 }
 
 function generateRandomStringId(): string {
@@ -19,12 +25,30 @@ function generateRandomStringId(): string {
   );
 }
 
+function addNode(state: TUseDirectedGraph, node: Node) {
+  if (state.nodes.has(node.id)) {
+    const newNodes = new Map(state.nodes);
+    newNodes.set(node.id, node);
+
+    const newAdjacencyList = new Map(state.adjacencyList);
+    newAdjacencyList.set(node.id, [])
+
+    return { nodes: newNodes, adjacencyList: newAdjacencyList }
+  }
+}
+
+function removeNode(state: TUseDirectedGraph, nodeId: string) {
+  const newNodes = new Map(state.nodes);
+  
+}
+
 export const useDirectedGraph = create((set) => ({
-    graph: {
-        nodes: new Map<string, Node>(),
-        adjacencyList: new Map<string, Edge[]>()
-    },
-    addNode: (node: Node) =>{
-        
-    }
-}))
+  nodes: new Map<string, Node>(),
+  adjacencyList: new Map<string, Edge[]>(),
+  addNode: (node: Node) => {
+    set((state: TUseDirectedGraph) => addNode(state, node));
+  },
+  removeNode: (nodeId: string) => {
+    set((state: TUseDirectedGraph) => removeNode(state, nodeId));
+  }
+}));
