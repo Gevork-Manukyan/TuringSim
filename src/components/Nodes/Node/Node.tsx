e0616aba6fc5f307c5b06fb0bb9e5d0280712ec6
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2024-07-24 14:05:32 -0700

    plusbutton annimations

--- src/components/Nodes/Node/Node.tsx
+++ src/components/Nodes/Node/Node.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Node.scss'
 
 type NodeProps = {
@@ -6,16 +7,25 @@ type NodeProps = {
 }
 
 export default function Node({ children, className }: NodeProps) {
+  const [isClicked, setIsClicked] = useState(false);
+  
+  const handleClick = () => {
+    setIsClicked(prev => !prev)
+  }
+
   return (
     <div className='Node__wrapper'>
-      <span className="Node__plus_btn">+</span>
-      <span className="Node__plus_btn">+</span>
-      <span className="Node__plus_btn">+</span>
-      <span className="Node__plus_btn">+</span>
-      
-      <button className={`Node${className ? ` ${className}` : ''}`}>
+      <button 
+        className={`Node${className ? ` ${className}` : ''}${isClicked ? ' Node--clicked': ''}`} 
+        onClick={handleClick}
+      >
         {children}
       </button>
+
+      <span className="Node__plus_btn Node__plus_btn--1">+</span>
+      <span className="Node__plus_btn Node__plus_btn--2">+</span>
+      <span className="Node__plus_btn Node__plus_btn--3">+</span>
+      <span className="Node__plus_btn Node__plus_btn--4">+</span>
     </div>
   )
 }
