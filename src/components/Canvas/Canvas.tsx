2e506bc3395f3878c4c703f0cfa98901c078539c
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2024-07-26 01:36:51 -0700

    makes node draggable and changes left click to right click

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -4,6 +4,7 @@ import Node from "../Nodes/Node/Node";
 import EndNode from "../Nodes/EndNode/EndNode";
 import AddNodeButton from "../Buttons/AddNodeButton/AddNodeButton";
 import Arrow from "../Arrow/Arrow";
+import { DndContext } from "@dnd-kit/core";
 
 export default function Canvas() {
   const graphNodes = useDirectedGraph((state) => state.nodes)
@@ -11,12 +12,14 @@ export default function Canvas() {
 
 
   return (
-    <section id="Canvas">
-      <Arrow startPoint={{x: 0, y: 0}} endPoint={{x: 382, y: 418}} />
-      {isGraphEmpty() && <AddNodeButton>+</AddNodeButton>}
-      {Array.from(graphNodes.entries()).map(([key, node]) => {
-        return <Node key={key} node={node} />
-      })}
-    </section>
+    <DndContext>
+      <section id="Canvas">
+        {/* <Arrow startPoint={{x: 0, y: 0}} endPoint={{x: 382, y: 418}} /> */}
+        {isGraphEmpty() && <AddNodeButton>+</AddNodeButton>}
+        {Array.from(graphNodes.entries()).map(([key, node]) => {
+          return <Node key={key} node={node} />
+        })}
+      </section>
+    </DndContext>
   );
 }
--- src/components/Nodes/Node/Node.tsx
+++ src/components/Nodes/Node/Node.tsx
@@ -2,6 +2,7 @@ import './Node.scss'
 import { useEffect, useRef, useState } from 'react';
 import { useDirectedGraph, TNode } from '../../../lib/hooks/useDirectedGraph';
 import { Point } from '../../Arrow/Arrow';
+import { useDraggable } from '@dnd-kit/core';
 
 type NodeProps = {
   className?: string;
@@ -11,24 +12,38 @@ type NodeProps = {
 export default function Node({ className, node }: NodeProps) {
   const [isClicked, setIsClicked] = useState(false);
   const [coordinates, setCoordinates] = useState<Point>({x: 0, y: 0})
-  const ref: React.LegacyRef<HTMLDivElement> | undefined = useRef(null);
   
-  const handleClick = () => {
+  const {attributes, listeners, setNodeRef, transform, isDragging} = useDraggable({
+    id: 'draggable',
+  });
+  const style = transform ? {
+    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+  } : undefined;
+
+  const handleRightClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
     setIsClicked(prev => !prev)
   }
 
   useEffect(() => {
-    if (ref.current) {
-      const rect = ref.current.getBoundingClientRect();
-      setCoordinates({ x: rect.x, y: rect.y })
-    }
-  }, [])
+    if (isDragging) setIsClicked(false)
+  }, [isDragging])
+
+  // useEffect(() => {
+  //   if (ref.current) {
+  //     const rect = ref.current.getBoundingClientRect();
+  //     setCoordinates({ x: rect.x, y: rect.y })
+  //   }
+  // }, [])
 
   return (
-    <div className='Node__wrapper' ref={ref}>
+    <div className='Node__wrapper' style={style}>
       <button 
-        className={`Node${className ? ` ${className}` : ''}${isClicked ? ' Node--clicked': ''}`} 
-        onClick={handleClick}
+        className={`Node${className ? ` ${className}` : ''}${isClicked && !isDragging ? ' Node--clicked': ''}`} 
+        ref={setNodeRef}
+        {...attributes}
+        {...listeners}
+        onContextMenu={handleRightClick}
       >
         {node.value ? node.value : "Node"}
       </button>
