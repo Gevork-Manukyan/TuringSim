0c40179a8847d2f6d39d5e936c8f979ceac922db
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2024-08-08 16:22:09 -0700

    adds getEdgeCoords to useConnectNodes

--- src/lib/stores/useConnectNodes.ts
+++ src/lib/stores/useConnectNodes.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 import { Coords, TNode } from "../types";
+import { TEdgeCoords } from "./useDirectedGraph";
+import { calcEdgeCoords } from "../util";
 
 type TUseConnectNodes = {
     isAddingEdge: boolean;
@@ -10,9 +12,10 @@ type TUseConnectNodes = {
     setStartNode: (node: TUseConnectNodes['startNode']) => void;
     setEndNode: (node: TUseConnectNodes['endNode']) => void;
     setMouseCoords: (coords: TUseConnectNodes['mouseCoords']) => void;
+    getEdgeCoords: () => TEdgeCoords | null;
 }
 
-export const useConnectNodes = create<TUseConnectNodes>((set) => ({
+export const useConnectNodes = create<TUseConnectNodes>((set, get) => ({
     isAddingEdge: false,
     startNode: null,
     endNode: null,
@@ -20,5 +23,17 @@ export const useConnectNodes = create<TUseConnectNodes>((set) => ({
     setIsAddingEdge: (value) => set({ isAddingEdge: value }),
     setStartNode: (node) => set({ startNode: node }),
     setEndNode: (node) => set({ endNode: node }),
-    setMouseCoords: (coords) => set({ mouseCoords: coords })
+    setMouseCoords: (coords) => set({ mouseCoords: coords }),
+    getEdgeCoords: () => {
+        const startNode = get().startNode;
+        const endNode = get().endNode;
+        
+        if (startNode && endNode) getEdgeCoords(startNode.coords, endNode.coords)
+        
+        return null;  // Return null if either startNode or endNode is null
+    }
 }))
+
+function getEdgeCoords(startNodeCoords: Coords, endNodeCoords: Coords) {
+    return calcEdgeCoords({ startCoords: startNodeCoords, endCoords: endNodeCoords })
+}
