21398a792a5dd32bab5ca4b3531cead28af7ef86
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2024-08-12 13:40:04 -0700

    abstract Arrow functions to util file

--- src/components/Arrow/Arrow.tsx
+++ src/components/Arrow/Arrow.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import { ArrowConfig, Coords } from "../../lib/types";
+import { calculateDeltas, calculateControlPointsWithBuffer, calculateCanvasDimensions, calculateArrowheadPoints } from './util';
 
 // Types
 type Props = {
@@ -113,110 +114,8 @@ const Arrow = ({
     arrowHeadEndingSize +
     dotEndingRadius;
 
-  const calculateDeltas = (startPoint: Coords, targetPoint: Coords) => {
-    const dx = targetPoint.x - startPoint.x;
-    const dy = targetPoint.y - startPoint.y;
-    const absDx = Math.abs(dx);
-    const absDy = Math.abs(dy);
-
-    return { dx, dy, absDx, absDy };
-  };
-
-  const calculateControlPoints = ({
-    absDx,
-    absDy,
-    dx,
-    dy,
-  }: {
-    absDx: number;
-    absDy: number;
-    dx: number;
-    dy: number;
-  }) => {
-    let leftTopX = 0;
-    let leftTopY = 0;
-    let rightBottomX = absDx;
-    let rightBottomY = absDy;
-    if (dx < 0) [leftTopX, rightBottomX] = [rightBottomX, leftTopX];
-    if (dy < 0) [leftTopY, rightBottomY] = [rightBottomY, leftTopY];
-
-    const p1 = { x: leftTopX, y: leftTopY };
-    const p2 = { x: rightBottomX, y: rightBottomY };
-
-    return { p1, p2 };
-  };
-
-  const calculateControlPointsWithBuffer = ({
-    boundingBoxElementsBuffer,
-    absDx,
-    absDy,
-    dx,
-    dy,
-  }: {
-    boundingBoxElementsBuffer: number;
-    absDx: number;
-    absDy: number;
-    dx: number;
-    dy: number;
-  }) => {
-    const { p1, p2 } = calculateControlPoints({
-      absDx,
-      absDy,
-      dx,
-      dy,
-    });
-
-    const topBorder = Math.min(p1.y, p2.y);
-    const bottomBorder = Math.max(p1.y, p2.y);
-    const leftBorder = Math.min(p1.x, p2.x);
-    const rightBorder = Math.max(p1.x, p2.x);
-
-    const verticalBuffer =
-      (bottomBorder - topBorder - absDy) / 2 + boundingBoxElementsBuffer;
-    const horizontalBuffer =
-      (rightBorder - leftBorder - absDx) / 2 + boundingBoxElementsBuffer;
-
-    return {
-      p1: { x: p1.x + horizontalBuffer, y: p1.y + verticalBuffer },
-      p2: { x: p2.x + horizontalBuffer, y: p2.y + verticalBuffer },
-      boundingBoxBuffer: { vertical: verticalBuffer, horizontal: horizontalBuffer },
-    };
-  };
-
-  const calculateCanvasDimensions = ({
-    absDx,
-    absDy,
-    boundingBoxBuffer,
-  }: {
-    absDx: number;
-    absDy: number;
-    boundingBoxBuffer: { vertical: number; horizontal: number };
-  }) => {
-    const canvasWidth = absDx + 2 * boundingBoxBuffer.horizontal;
-    const canvasHeight = absDy + 2 * boundingBoxBuffer.vertical;
-
-    return { canvasWidth, canvasHeight };
-  };
-
-  const calculateArrowheadPoints = ({ p2, arrowHeadEndingSize, angle }: { p2: Coords, arrowHeadEndingSize: number, angle: number }) => {
-    const arrowPoint1 = {
-      x: p2.x - arrowHeadEndingSize * Math.cos(angle) + arrowHeadEndingSize * Math.sin(angle),
-      y: p2.y - arrowHeadEndingSize * Math.sin(angle) - arrowHeadEndingSize * Math.cos(angle),
-    };
-    
-    const arrowPoint2 = {
-      x: p2.x - arrowHeadEndingSize * Math.cos(angle) - arrowHeadEndingSize * Math.sin(angle),
-      y: p2.y - arrowHeadEndingSize * Math.sin(angle) + arrowHeadEndingSize * Math.cos(angle),
-    };
-
-    return { arrowPoint1, arrowPoint2 }
-  }
-
-  // absDx/Dy is the x/y distance between the start and end
-  // dx/dy are used to know the quadrant (ex. if dx is positive then you know its on the right side so Q1 or Q4)
   const { absDx, absDy, dx, dy } = calculateDeltas(startPoint, endPoint);
-  // p1 represents the top left corner 
-  // p2 represents the bottom right corner
+
   const { p1, p2, boundingBoxBuffer } = calculateControlPointsWithBuffer({
     boundingBoxElementsBuffer,
     dx,
---
+++ src/components/Arrow/util.ts
@@ -0,0 +1,100 @@
+import { Coords } from "../../lib/types";
+
+export const calculateDeltas = (startPoint: Coords, targetPoint: Coords) => {
+    const dx = targetPoint.x - startPoint.x;
+    const dy = targetPoint.y - startPoint.y;
+    const absDx = Math.abs(dx);
+    const absDy = Math.abs(dy);
+
+    return { dx, dy, absDx, absDy };
+  };
+
+export const calculateControlPoints = ({
+    absDx,
+    absDy,
+    dx,
+    dy,
+  }: {
+    absDx: number;
+    absDy: number;
+    dx: number;
+    dy: number;
+  }) => {
+    let leftTopX = 0;
+    let leftTopY = 0;
+    let rightBottomX = absDx;
+    let rightBottomY = absDy;
+    if (dx < 0) [leftTopX, rightBottomX] = [rightBottomX, leftTopX];
+    if (dy < 0) [leftTopY, rightBottomY] = [rightBottomY, leftTopY];
+
+    const p1 = { x: leftTopX, y: leftTopY };
+    const p2 = { x: rightBottomX, y: rightBottomY };
+
+    return { p1, p2 };
+  };
+
+export const calculateControlPointsWithBuffer = ({
+    boundingBoxElementsBuffer,
+    absDx,
+    absDy,
+    dx,
+    dy,
+  }: {
+    boundingBoxElementsBuffer: number;
+    absDx: number;
+    absDy: number;
+    dx: number;
+    dy: number;
+  }) => {
+    const { p1, p2 } = calculateControlPoints({
+      absDx,
+      absDy,
+      dx,
+      dy,
+    });
+
+    const topBorder = Math.min(p1.y, p2.y);
+    const bottomBorder = Math.max(p1.y, p2.y);
+    const leftBorder = Math.min(p1.x, p2.x);
+    const rightBorder = Math.max(p1.x, p2.x);
+
+    const verticalBuffer =
+      (bottomBorder - topBorder - absDy) / 2 + boundingBoxElementsBuffer;
+    const horizontalBuffer =
+      (rightBorder - leftBorder - absDx) / 2 + boundingBoxElementsBuffer;
+
+    return {
+      p1: { x: p1.x + horizontalBuffer, y: p1.y + verticalBuffer },
+      p2: { x: p2.x + horizontalBuffer, y: p2.y + verticalBuffer },
+      boundingBoxBuffer: { vertical: verticalBuffer, horizontal: horizontalBuffer },
+    };
+  };
+
+export const calculateCanvasDimensions = ({
+    absDx,
+    absDy,
+    boundingBoxBuffer,
+  }: {
+    absDx: number;
+    absDy: number;
+    boundingBoxBuffer: { vertical: number; horizontal: number };
+  }) => {
+    const canvasWidth = absDx + 2 * boundingBoxBuffer.horizontal;
+    const canvasHeight = absDy + 2 * boundingBoxBuffer.vertical;
+
+    return { canvasWidth, canvasHeight };
+  };
+
+export const calculateArrowheadPoints = ({ p2, arrowHeadEndingSize, angle }: { p2: Coords, arrowHeadEndingSize: number, angle: number }) => {
+    const arrowPoint1 = {
+      x: p2.x - arrowHeadEndingSize * Math.cos(angle) + arrowHeadEndingSize * Math.sin(angle),
+      y: p2.y - arrowHeadEndingSize * Math.sin(angle) - arrowHeadEndingSize * Math.cos(angle),
+    };
+    
+    const arrowPoint2 = {
+      x: p2.x - arrowHeadEndingSize * Math.cos(angle) - arrowHeadEndingSize * Math.sin(angle),
+      y: p2.y - arrowHeadEndingSize * Math.sin(angle) + arrowHeadEndingSize * Math.cos(angle),
+    };
+
+    return { arrowPoint1, arrowPoint2 }
+  }
