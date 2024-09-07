747fef9d6c9e5bcf236eeae99f268361bd484c75
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-09-07 12:51:03 -0700

    creates input field for string and comma separated option

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,12 +1,33 @@
 import "./Toolbar.scss"
 import { useDirectedGraph } from "../../lib/stores/useDirectedGraph"
+import { useState } from "react"
 
 export default function Toolbar() {
+    const [inputString, setInputString] = useState("")
+    const [options, setOptions] = useState({
+        commaSeparated: false,
+    })
+    
     const evaluate = useDirectedGraph(state => state.evaluate)
+
+    const handleEvaluateClick = () => {
+        const evaluateString = options.commaSeparated ? inputString.split(', ') : inputString.split('');
+        evaluate(evaluateString)
+    }
+
+    const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, checked } = event.target
+        setOptions(prev => ({ ...prev, [name]: checked }))
+    }
     
     return (
         <section className="Toolbar">
-            <button onClick={() => evaluate(['1', '2', '3'])}>Start</button>
+            <button onClick={handleEvaluateClick}>Start</button>
+            <input id="Toolbar__stringInput" value={inputString} onChange={(e) => setInputString(e.target.value)}></input>
+            <label className="Toolbar__label" htmlFor="commaSeparated">
+                Comma Separated
+                <input className="Toolbar__checkbox" name="commaSeparated" type="checkbox" onChange={handleOptionChange}></input>
+            </label>
         </section>
     )
 }
